cea620956f24f16e68419999b0f788c9f054e19f
Klaus Harbo <klaus@harbo.net>
2022-06-22 19:28:07 +0200

    Reintroduce devshell

--- flake.nix
+++ flake.nix
@@ -1,31 +1,78 @@
 {
   description = "Chinook database (SQLite version)";
   inputs = {
+    devshell = { url = "github:numtide/devshell/f55e05c";  inputs.nixpkgs.follows = "nixpkgs"; }; # 2022-06-10
     flake-utils.url  = "github:numtide/flake-utils/v1.0.0";
     nixpkgs.url      = "github:NixOS/nixpkgs/22.05";
   };
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, devshell, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
+      let version = "0.0.1";
+          datadir = "data/sqlite";
+          chindb  = "${datadir}/chinook.db";
+          northdb = "${datadir}/northwind.db";
+      in
       rec {
-        pkgs = import nixpkgs { inherit system; };
-
-        packages =  rec {
-          sqlite = pkgs.stdenv.mkDerivation {
-            inherit system;
-            sqlite = pkgs.sqlite;
-            pname = "chinook-database-sqlite";
-            version = "0.0.1";
-            src = builtins.fetchGit {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ devshell.overlay ];
+        };
+        sqlite = pkgs.sqlite;
+        packages = rec {
+          default = sqlite-chinook;
+          sqlite-chinook = pkgs.stdenv.mkDerivation {
+            inherit chindb sqlite system version;
+            pname = "chinook-sqlite";
+          src = builtins.fetchGit {
               url = "https://github.com/lerocha/chinook-database";
               rev = "e7e6d5f008e35d3f89d8b8a4f8d38e3bfa7e34bd";
             };
             installPhase = ''
               data_dir=$out/data/sqlite
               mkdir -p $data_dir
-              $sqlite/bin/sqlite3 -init $src/ChinookDatabase/DataSources/Chinook_Sqlite.sql $data_dir/chinook-sqlite3.db
+              $sqlite/bin/sqlite3 -init $src/ChinookDatabase/DataSources/Chinook_Sqlite.sql $out/$chindb
             '';
           };
-          default = sqlite;
+          sqlite-northwind = pkgs.stdenv.mkDerivation {
+            inherit northdb sqlite system datadir;
+            pname = "northwind-sqlite";
+            version = "0.0.1";
+            src = builtins.fetchGit {
+              url = "https://github.com/jpwhite3/northwind-SQLite3";
+              rev = "46d5f8a64f396f87cd374d1600dbf521523980e8";
+            };
+            installPhase = ''
+              mkdir -p $out/$datadir
+              $sqlite/bin/sqlite3 -init $src/Northwind.Sqlite3.create.sql $out/$northdb
+            '';
+          };
         };
+        #--------------------------------------------------------------------------------
+        # DevShell
+        #--------------------------------------------------------------------------------
+        devShell = self.devShells.${system}.default;
+        devShells.default =
+          with pkgs;
+          pkgs.devshell.mkShell {
+            packages = [
+              curl
+              git
+              ripgrep
+              tree
+              unixtools.top
+              unixtools.watch
+              wget
+            ];
+            commands = let
+              cat = "Build SQLite databases";
+            in [{name = "build-chinook";
+                 help = "Build Chinook SQLite database";
+                 command = "nix build .#sqlite-chinook && install -m644 result/${chindb} . && rm -f result";
+                 category = cat;}
+                {name = "build-northwind";
+                 help = "Build Norhtwind SQLite database";
+                 command = "nix build .#sqlite-northwind && install -m644 result/${northdb} . && rm -f result";
+                 category = cat;}];
+          };
       });
 }
