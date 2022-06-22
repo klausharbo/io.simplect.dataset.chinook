fdd38014f3f6389a89baf91568451a1524cf29b1
Klaus Harbo <klaus@harbo.net>
2022-06-22 18:38:43 +0200

    Remove devShell (not needed)

--- flake.nix
+++ flake.nix
@@ -1,57 +1,31 @@
 {
-  description = "Remorph";
+  description = "Chinook database (SQLite version)";
   inputs = {
-    devshell = { url = "github:numtide/devshell/f55e05c";  inputs.nixpkgs.follows = "nixpkgs"; }; # 2022-06-10
     flake-utils.url  = "github:numtide/flake-utils/v1.0.0";
     nixpkgs.url      = "github:NixOS/nixpkgs/22.05";
   };
-  outputs = { self, devshell, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       rec {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlay ];
-        };
+        pkgs = import nixpkgs { inherit system; };
 
-        #--------------------------------------------------------------------------------
-        # Packages
-        #--------------------------------------------------------------------------------
         packages =  rec {
           sqlite = pkgs.stdenv.mkDerivation {
             inherit system;
             sqlite = pkgs.sqlite;
-            name = "chinook-sqlite";
+            pname = "chinook-database-sqlite";
+            version = "0.0.1";
             src = builtins.fetchGit {
               url = "https://github.com/lerocha/chinook-database";
-              ref = "master";
               rev = "e7e6d5f008e35d3f89d8b8a4f8d38e3bfa7e34bd";
             };
             installPhase = ''
-              mkdir $out
-              $sqlite/bin/sqlite3 -init $src/ChinookDatabase/DataSources/Chinook_Sqlite.sql $out/chinook-sqlite3.db
+              data_dir=$out/data/sqlite
+              mkdir -p $data_dir
+              $sqlite/bin/sqlite3 -init $src/ChinookDatabase/DataSources/Chinook_Sqlite.sql $data_dir/chinook-sqlite3.db
             '';
           };
           default = sqlite;
         };
-
-        #--------------------------------------------------------------------------------
-        # DevShell
-        #--------------------------------------------------------------------------------
-        devShell = self.devShells.${system}.default;
-        devShells.default =
-          with pkgs;
-          pkgs.devshell.mkShell {
-            packages = [
-              curl
-              git
-              ripgrep
-              tree
-              unixtools.top
-              unixtools.watch
-              wget
-            ];
-            commands =
-                [];
-          };
       });
 }
